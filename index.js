a60efe28e473dd0b9cd18bb0a01142479a7fd271
Agatha Lafaietty <agatha.lafaietyy@gmail.com>
2021-06-25 17:14:52 -0300

    commit

--- index.js
+++ index.js
@@ -48,50 +48,58 @@ const bancoCadastrado = {
       }
     ]
   }
-
+  
   function acessar() {
     const erros = verificarDados()
     if (erros.length > 0) {
-        for (let posicao = 0; posicao <= erros.length; posicao++) {
-            alert(erros[posicao])        
-        }
+      for (let posicao = 0; posicao < erros.length; posicao++) {
+        document.getElementById(erros[posicao].id).innerHTML = erros[posicao].message
+        document.getElementById(erros[posicao].id.substring(0, erros[posicao].id.length - 5)).classList.add("input-error")
+      }
     } else {
-        alert("Login Liberado")
+      alert("Login liberado")
     }
   }
   
   function verificarDados() {
-    var agenciaDigitada
-    var contaDigitada
-    var senhaDigitada
+    var agenciaDigitada = document.getElementById("agencia").value
+    var contaDigitada = document.getElementById("conta").value
+    var senhaDigitada = document.getElementById("senha").value
     var agenciaEncontrada
     var contaEncontrada
     var erros = []
-  
-    for (let posicaoAtual = 0; posicaoAtual <= bancoCadastrado.agencias.length; posicaoAtual++) {
+    for (let posicaoAtual = 0; posicaoAtual < bancoCadastrado.agencias.length; posicaoAtual++) {
       if (agenciaDigitada == bancoCadastrado.agencias[posicaoAtual].numero) {
         agenciaEncontrada = bancoCadastrado.agencias[posicaoAtual]
       }
     }
   
     if (!agenciaEncontrada) {
-      erros.push('Agencia nao encontrada, digite novamente.')
+      erros.push({
+        id: "agenciaError",
+        message: 'Agencia nao encontrada, digite novamente.'
+      })
     }
   
-    for (let posicaoAtual = 0; posicaoAtual <= agenciaEncontrada?.contas.length; posicaoAtual++) {
+    for (let posicaoAtual = 0; posicaoAtual < agenciaEncontrada?.contas.length; posicaoAtual++) {
       if (contaDigitada == agenciaEncontrada.contas[posicaoAtual].numero) {
         contaEncontrada = agenciaEncontrada.contas[posicaoAtual]
       }
     }
   
     if (!contaEncontrada) {
-      erros.push('Conta nao encontrada, digite novamente.')
+      erros.push({
+        id: "contaError",
+        message: 'Conta nao encontrada, digite novamente.'
+      })
     }
   
     if (senhaDigitada != contaEncontrada?.senha) {
-      erros.push('Senha invalida, digite novamente') 
+      erros.push({
+        id: "senhaError",
+        message: 'Senha invalida, digite novamente'
+      })
     }
-
+  
     return erros
   }
-
