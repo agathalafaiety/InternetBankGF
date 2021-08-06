98e330ecbc4c3095bd33869e2d359620a003b10b
Agatha Lafaiety <agatha.lafaietyy@gmail.com>
2021-08-06 14:16:40 -0300

    commit

--- Cadastro/index.js
+++ Cadastro/index.js
@@ -1,21 +1,33 @@
-const bancoCadastrado = pegarDadosDoLocalStorege()
-console.log(bancoCadastrado)
+const bancoCadastrado = pegarDadosDoLocalStorage()
+var erros = [];
+var nomeDigitado
+var cpfDigitado
 
-var nomeDigitado;
-var cpfDigitado;
-var idadeDigitada;
-var emailDigitado;
-var sexoDigitado;
-var senhaDigitada;
-var confirmacaoDigitada;
+const criarConta = () => {
+  var idadeDigitada = document.getElementById("idade").value;
+  var emailDigitado = document.getElementById("email").value;
+  var sexoSelecionado = document.getElementById("sexo").value;
+  var senhaDigitada = document.getElementById("senha").value;
+  var confirmacaoDigitada = document.getElementById("confirmacaoSenha").value;
+}
 
-const validarCpf = (cpf) => {
-  bancoCadastro.agencias.forEach(agencia => {
-    const contaEncontrada = agencia.contas.find(conta => cpf === conta.usuario.cpf)
-    if (contaEncontrada) {
-      alert("CPF já foi cadastrado por outro usúario, digite outro CPF")
+const validarCpf = () => {
+  cpfDigitado = document.getElementById("cpf").value;
+  var contaEncontrada
+
+  bancoCadastrado.agencias.forEach(agencia => {
+    if (agencia.contas.find(conta => cpfDigitado == conta.usuario.cpf)) {
+      contaEncontrada = agencia.contas.find(conta => cpfDigitado == conta.usuario.cpf)
     }
   })
+  
+  if (contaEncontrada) {
+    erros.push["cpf"]
+    document.getElementById("cpfError").innerHTML = 'CPF já foi cadastrado por outro usúario, digite outro CPF'
+  } else {
+    erros = erros.filter(erro => erro !== "cpf")
+    document.getElementById("cpfError").innerHTML = ''
+  }
 }
 
 function validarEmail(email) {
@@ -33,14 +45,19 @@ const validarSenha = (senha, confirmacaoSenha) => {
   }
 }
 
-const validarNome = (nome) => {
-  if(nome.length < 2 ){
-    alert("Nome inválido, digite novamente")
+const validarNome = () => {
+  nomeDigitado = document.getElementById("nome").value;
+  if (nomeDigitado.length <= 2) {
+    erros.push["nome"]
+    document.getElementById("nomeError").innerHTML = 'Nome inválido, digite novamente'
+  } else {
+    erros = erros.filter(erro => erro !== "nome")
+    document.getElementById("nomeError").innerHTML = ''
   }
 }
 
 const validarIdade = (idade) => {
-  if(idade < 18){
+  if (idade < 18) {
     alert("A idade minima é 18 anos")
   }
 }
--- index.js
+++ index.js
@@ -1,133 +1,24 @@
-<<<<<<< HEAD
-salvarDadosNoLocalStorege();
-const banco = pegarDadosNoLocalStorege();
 
+salvarDadosNoLocalStorage();
+const bancoCadastrado = pegarDadosDoLocalStorage()
+  
 function criarConta() {
   window.location.href = "Cadastro/index.html"
 }
 
 function acessar() {
-  var contasComSaldoMaiorQue500 = [];
-  bancoCadastrado.agencias.forEach((agencia) => {
-    let contasEncontradas = agencia.contas.filter(
-      (conta) => conta.saldo >= 500
-    );
-    contasEncontradas.forEach((contaEncontrada) => {
-      contasComSaldoMaiorQue500.push(contaEncontrada);
-    });
-  });
-  console.log(contasComSaldoMaiorQue500);
-
-  var sexoF = [];
-  bancoCadastrado.agencias.forEach((agencia) => {
-    let contasEncontradas = agencia.contas.filter(
-      (conta) => (conta.usuario.sexo = "F")
-    );
-    contasEncontradas.forEach((contaEncontrada) => {
-      sexoF.push(contaEncontrada);
-    });
-  });
-  console.log(sexoF);
-
-  const erros = verificarDados();
-  if (erros.length > 0) {
-    erros.forEach((erro) => {
-      document.getElementById(erro.id).innerHTML = erro.message;
-      document
-        .getElementById(erro.id.substring(0, erro.id.length - 5))
-        .classList.add("input-error");
-    });
-  } else {
-    alert("Login liberado");
-=======
-const bancoCadastrado = {
-  logo: 'Logo',
-  nome: 'Internet Bank GF',
-  agencias: [
-    {
-      numero: 1212,
-      contas: [{
-        numero: 1234,
-        senha: 4321,
-        saldo: 500,
-        usuario: {
-          nome: 'Paulo',
-          idade: 30,
-          sexo: 'M',
-          email: 'paulo@gmail.com',
-          cpf: '123.456.789-10'
-        }
-      },
-      {
-        numero: 6789,
-        senha: 9876,
-        saldo: 1000,
-        usuario: {
-          nome: 'José',
-          idade: 20,
-          sexo: 'M',
-          email: 'jose@gmail.com',
-          cpf: '123.456.789-15'
-        }
-      }
-      ]
-    },
-    {
-      numero: 1213,
-      contas: [{
-        numero: 1234,
-        senha: 4321,
-        saldo: 500,
-        usuario: {
-          nome: 'Ana',
-          idade: 80,
-          sexo: 'F',
-          email: 'ana@gmail.com',
-          cpf: '123.456.788-10'
-        }
-      }
-      ]
-    }
-  ]
-}
-
-function acessar() {
-  var sexoF = []
-  bancoCadastrado.agencias.forEach(agencia => {
-    let contasEncontradas = agencia.contas.filter( conta => conta.usuario.sexo = 'F')
-    contasEncontradas.forEach(contaEncontrada => {
-      sexoF.push(contaEncontrada)
-    })
-  })
-  console.log(sexoF)
-}
-
-function acessar() {
   const erros = verificarDados()
-  if () {
+  if (erros.length > 0) {
     erros.forEach(erro => {
       document.getElementById(erro.id).innerHTML = erro.message
       document.getElementById(erro.id.substring(0, erro.id.length - 5)).classList.add("input-error")
     })
   } else {
     alert("Login liberado")
->>>>>>> 2677e25c53b108c5ad2179adb2f3a57c16c0b57e
   }
 }
 
 function verificarDados() {
-<<<<<<< HEAD
-  var agenciaDigitada = document.getElementById("agencia").value;
-  var contaDigitada = document.getElementById("conta").value;
-  var senhaDigitada = document.getElementById("senha").value;
-  var agenciaEncontrada;
-  var contaEncontrada;
-  var erros = [];
-
-  agenciaEncontrada = bancoCadastrado.agencias.find(
-    (agencia) => agenciaDigitada == agencia.numero
-  );
-=======
   var agenciaDigitada = document.getElementById("agencia").value
   var contaDigitada = document.getElementById("conta").value
   var senhaDigitada = document.getElementById("senha").value
@@ -135,56 +26,32 @@ function verificarDados() {
   var contaEncontrada
   var erros = []
 
-  agenciaEncontrada = bancoCadastrado.agencias.find( agencia => agenciaDigitada == agencia.numero)
->>>>>>> 2677e25c53b108c5ad2179adb2f3a57c16c0b57e
+  agenciaEncontrada = bancoCadastrado.agencias.find(
+    agencia => agenciaDigitada == agencia.numero)
 
   if (!agenciaEncontrada) {
     erros.push({
       id: "agenciaError",
-<<<<<<< HEAD
-      message: "Agencia nao encontrada, digite novamente.",
-    });
-  }
-
-  contaEncontrada = agenciaEncontrada?.contas.find(
-    (conta) => contaDigitada == conta.numero
-  );
-=======
       message: 'Agencia nao encontrada, digite novamente.'
     })
   }
 
-contaEncontrada = agenciaEncontrada?.contas.find(conta => 
-    contaDigitada == conta.numero) 
->>>>>>> 2677e25c53b108c5ad2179adb2f3a57c16c0b57e
+  contaEncontrada = agenciaEncontrada?.contas.find
+    (conta => contaDigitada == conta.numero)
 
   if (!contaEncontrada) {
     erros.push({
       id: "contaError",
-<<<<<<< HEAD
-      message: "Conta nao encontrada, digite novamente.",
-    });
-=======
       message: 'Conta nao encontrada, digite novamente.'
     })
->>>>>>> 2677e25c53b108c5ad2179adb2f3a57c16c0b57e
   }
 
   if (senhaDigitada != contaEncontrada?.senha) {
     erros.push({
       id: "senhaError",
-<<<<<<< HEAD
-      message: "Senha invalida, digite novamente",
-    });
-  }
-
-  return erros;
-}
-=======
       message: 'Senha invalida, digite novamente'
     })
   }
 
   return erros
 }
->>>>>>> 2677e25c53b108c5ad2179adb2f3a57c16c0b57e
